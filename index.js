4f8a20fabdf6a76d312428c21c6e1a74ac96cc61
tai-shis <tshis070@mtroyal.ca>
2025-10-28 16:52:52 -0600

    header html and item data test added

--- index.js
+++ index.js
@@ -7,16 +7,40 @@ document.addEventListener('DOMContentLoaded', async function() {
     fetch(fetchURL)
       .then(response => response.json())
       .then(data => {
+        console.log(data)
         items = data;
         localStorage.setItem('items', JSON.stringify(items));
-        // render
+        render(items)
       })
     .catch(error => console.log(`Error fetching item data: ${error}`));
   } else {
     const items = JSON.parse(localStorage.getItem('items'));
-    // render
+    render(items)
   }
-    
+
+  // This functions should actually pre-render the entire page content, hiding everything except the home.
+  function render(items) {
+    const test = document.querySelector("#test");
+    for(const item of items) {
+      const div = document.createElement("div"); 
+        
+      div.textContent = `Item: ${item.name}, Gender: ${item.gender}, Category: ${item.category}`
+      test.appendChild(div)
+    }
+  }
+
+  // Might actually have to count all cart items, less efficient, but less error prone.
+  function addToCart() {
+    const cartCounter = document.querySelector("#cart-counter");
+    // might be able to just do this without parsing?
+    cartCounter.textContent = parseInt(cartCounter.textContent, 10) += 1;    
+  }
+
+  function removeFromCart() {
+    const cartCounter = document.querySelector("#cart-counter");
+    cartCounter.textContent = parseInt(cartCounter.textContent, 10) += 1;
+  }
 
 
 });
+
